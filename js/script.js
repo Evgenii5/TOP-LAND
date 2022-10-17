3455f8a4e81c113eebcaa947b250eef308b7a740
DiPokemon <dimitrnklnk@gmail.com>
2022-10-17 22:05:15 +0300

    рабочий аккордеон

--- js/script.js
+++ js/script.js
@@ -1,34 +1,23 @@
-/*var coll = document.getElementsByClassName ("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-    coll(i).addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-    }   else {
-        content.style.maxHeight = content.scrollHeight + "px";
+/*var accItem = document.getElementsByClassName('accordion__item');
+var accHD = document.getElementsByClassName('accordion__item_header');
+
+for (i = 0; i < accHD.length; i++) {
+    accHD[i].addEventListener('click', toggleItem, false);
+}
+function toggleItem() {
+    var itemClass = this.parentNode.className;
+    for (i = 0; i < accItem.length; i++) {
+        accItem[i].className = 'accordion__item close';
     }
-    });
+    if (itemClass == 'accordion__item close') {
+        this.parentNode.className = 'accordion__item open';
+    }
 }
 */
 
-/*находим все нужные нам headingElem*/
-let allElems = document.querySelectorAll('.accordion__header');
-/*прогоняем из через цикл*/
-allElems.forEach((elem) => {
-    /*вещаем на каждый элемент обработчик на событие click*/
-    elem.addEventListener('click', function () {
-        /*у нажатого элемента получаем родителя*/
-        let parentElem = this.parentNode;
-        /*находим элемент с контентом*/
-        let contentBlock = parentElem.querySelector('.accordion__body')
-        /*аналогичная проверка на наличие класса active*/
-        if (contentBlock.classList.contains('active')) {
-            contentBlock.classList.remove('active');
-        }
-        else {
-            contentBlock.classList.add('active');
-        }
-    })
-})
+
+$(document).ready(function () {
+    $('.accordion__item').click(function () {
+        $(this).toggleClass('open');
+    });
+});
